8d7eb02345a9701943e3fcb76ed35ad293e7d427
OdilovMuhammadRizo <odilovadiyora52@gmail.com>
2023-03-14 15:02:52 +0300

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import "./header.css";
+import Translation from "./Data.json";
 
 const Header = () => {
+  const [language, setLanguage] = useState(["UZB"]);
+  const [content, setContent] = useState({});
+
+  useEffect(() => {
+    if (language == "UZB") {
+      setContent(Translation.UZB);
+    } else if (language == "ENG") {
+      setContent(Translation.ENG);
+    } else if (language == "RUS") {
+      setContent(Translation.RUS);
+    }
+  });
+
   return (
     <div className="header__section">
       <header>
-        <div className="header__links">
+        <div className="header__links" >
           <ul>
             <li>
               <a href="#home">Bosh sahifa</a>
@@ -17,11 +31,16 @@ const Header = () => {
         </div>
 
         <div className="header__selection">
-            <select>
-                <option value="UZB">UZB</option>
-                <option value="EN">EN</option>
-                <option value="RUS">RUS</option>
-            </select>
+          <select
+            value={language}
+            onChange={(e) => {
+              setLanguage(e.target.value);
+            }}
+          >
+            <option value="UZB">UZB</option>
+            <option value="ENG">ENG</option>
+            <option value="RUS">RUS</option>
+          </select>
         </div>
       </header>
     </div>
